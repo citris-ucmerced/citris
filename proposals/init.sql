6ebb37f90b0b2151b0db4d43f258db6f63928921
Brian <brianhungerman@gmail.com>
2019-11-25 18:38:32 -0800

    Adding init sql and form labels to match

---
+++ proposals/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE forms
+(
+    project_title VARCHAR(100),
+    deadline DATETIME,
+    pi_name VARCHAR(100),
+    pi_email VARCHAR(100),
+    sponsor_name VARCHAR(100),
+    proposal_url VARCHAR(100),
+    preferred_submission VARCHAR(100),
+    opportunity_number VARCHAR(100),
+    nsf_proposal_number VARCHAR(100),
+    nsf_pin_number VARCHAR(100),
+    project_start DATETIME,
+    project_end DATETIME,
+    estimated_budget INT,
+    budget_items VARCHAR(1000),
+    extra_space BIT,
+    space_details VARCHAR(1000),
+    course_buyout BIT,
+    buyout_details VARCHAR(1000),
+    applicant_role VARCHAR(100),
+    additional_info VARCHAR(1000)
+);
+    
+
