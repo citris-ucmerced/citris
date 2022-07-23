7fef15f01dc2643e7f500a2c462df7599c270055
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-07-23 15:01:39 -0700

    fixed mobile image featured header and fixed undefined to CITRIS on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,8 @@
 				$(
 					'<div id="titleBar">' +
 						'<a href="#navPanel" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
+						// '<span class="title">' + $('#logo').html() + '</span>' +
+						'<span class="title">Citris</span>' +
 					'</div>'
 				)
 					.appendTo($body);
