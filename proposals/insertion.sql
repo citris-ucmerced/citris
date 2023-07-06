9547b6c41a213dbcddcf4e1c8215fe68f3b8447c
Alonso Ortiz <alonsoortiz73@gmail.com>
2023-07-06 14:40:05 -0700

    Clean up some unecessary files

--- proposals/insertion.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO forms VALUES (,,,,,,,,,,,,,,,,,,,,);
-
-String.Format(
-    "INSERT INTO forms VALUES ({0},{1},{2},...,,,,,,,,,,,,,,,,,);",
-    body.project_id,
-    body.project_title,
-    body.deadline,
-    ...
-)
