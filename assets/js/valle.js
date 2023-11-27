0b4361e329f25750836c3b760f29f29fba65cf0b
Adam Crawford <adamcrawford253@gmail.com>
2023-11-27 14:19:02 -0800

    Add 2023 poster to the previous posters.

--- assets/js/valle.js
+++ assets/js/valle.js
@@ -37,21 +37,29 @@ prevBtn.addEventListener("click", plusSlides);
 nextBtn.addEventListener("click", plusSlides);
 dots.forEach((dot) => dot.addEventListener("click", currentSlide));
 
-
 const myFunction = () => {
-  document.getElementById("first").style.display ='block';
-  document.getElementById("second").style.display ='none'
-  document.getElementById("third").style.display ='none'
-}
+  document.getElementById("first").style.display = "block";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+};
 
 const myFunction2 = () => {
-  document.getElementById("second").style.display ='block'
-  document.getElementById("first").style.display ='none'
-  document.getElementById("third").style.display ='none'
-}
+  document.getElementById("second").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+};
 
 const myFunction3 = () => {
-  document.getElementById("third").style.display ='block'
-  document.getElementById("first").style.display ='none'
-  document.getElementById("second").style.display ='none'
-}
+  document.getElementById("third").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+};
+const myFunction4 = () => {
+  document.getElementById("fourth").style.display = "block";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+};
